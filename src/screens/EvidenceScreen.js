701fb48f2715766c51a12e9c94b7fa60bd32af88
davidschlundtbodien <d.schlundtbodien@gmail.com>
2021-02-10 11:03:40 -0700

    adds FlatList and react native elements to evidence screen

--- src/screens/EvidenceScreen.js
+++ src/screens/EvidenceScreen.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet, Text, Button, FlatList } from 'react-native';
+import { CheckBox } from 'react-native-elements';
 
 const EvidenceScreen = ({ navigation }) => {
+  const evidence = [
+    {title: 'Ghost Writing'},
+    {title: 'EMF Level 5'},
+    {title: 'Freezing Temperatures'},
+    {title: 'Ghost Orbs'},
+    {title: 'Spirit Box'},
+    {title: 'Fingerprints'}
+  ];
+
   return (
     <>
       <Text>Evidence Screen</Text>
+      <FlatList
+        keyExtractor={(evidence) => evidence.title }
+        data={evidence}
+        renderItem={({ item }) => {
+          return <CheckBox 
+            title={item.title}
+          />;
+        }}
+      />
       <Button 
         title="Go to Ghost Screen"
         onPress={() => navigation.navigate('Ghost')}

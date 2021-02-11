3856a8bf3be5deceb58efaaf3596beec5f4c771a
davidschlundtbodien <d.schlundtbodien@gmail.com>
2021-02-11 16:05:01 -0700

    adds reducer and toggle function to evidence check boxes, removes flatlist

--- src/components/EvidenceCheckBox.js
+++ src/components/EvidenceCheckBox.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { CheckBox } from 'react-native-elements';
 
-const EvidenceCheckBox = (title) => {
+const EvidenceCheckBox = ({ evidence, dispatch }) => {
   return (
     <>
+      <CheckBox 
+        title={evidence.name}
+        checked={evidence.isChecked}
+        onPress={() => dispatch({type: 'check_box', payload: {id: evidence.id }})}
+      />
     </>
   );
 };
--- src/screens/EvidenceScreen.js
+++ src/screens/EvidenceScreen.js
@@ -1,29 +1,37 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, StyleSheet, Text, Button } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
 import EvidenceCheckBox from '../components/EvidenceCheckBox';
 
 const evidenceList = [
-  { name: 'Ghost Writing', isChecked: false},
-  { name: 'EMF Level 5', isChecked: false},
-  { name: 'Ghost Orbs', isChecked: false},
-  { name: 'Fingerprints', isChecked: false},
-  { name: 'Freezing Temps', isChecked: false},
-  { name: 'Spirit Box', isChecked: false}
+  {id: 1, name: 'Ghost Writing', isChecked: false},
+  {id: 2, name: 'EMF Level 5', isChecked: false},
+  {id: 3, name: 'Ghost Orbs', isChecked: false},
+  {id: 4, name: 'Fingerprints', isChecked: false},
+  {id: 5, name: 'Freezing Temps', isChecked: false},
+  {id: 6, name: 'Spirit Box', isChecked: false}
 ];
 
+const reducer = (evidence, action) => {
+  switch (action.type) {
+  case 'check_box':
+    return evidence.map(ev => {
+      if (ev.id === action.payload.id) {
+        return { ...ev, isChecked: !ev.isChecked };
+      }
+      return ev;
+    });
+  }
+};
+
 const EvidenceScreen = ({ navigation }) => {
+  const [evidence, dispatch] = useReducer(reducer, evidenceList);
 
   return (
     <>
       <Text>Evidence Screen</Text>
-      <FlatList
-        keyExtractor={evidence => evidence.name}
-        data={evidenceList}
-        renderItem={({ item }) => {
-          return <Text>{item.name}</Text>;
-        }}
-      />
+      {evidence.map(evidence => {
+        return <EvidenceCheckBox  key={evidence.id} evidence={evidence} dispatch={dispatch} />;
+      })}
       <Button 
         title="Go to Ghost Screen"
         onPress={() => navigation.navigate('Ghost')}

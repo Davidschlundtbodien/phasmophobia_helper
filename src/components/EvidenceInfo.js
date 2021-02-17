dc71abc215a6b9554fad7ab23c5c7b323fa9d68f
davidschlundtbodien <d.schlundtbodien@gmail.com>
2021-02-17 14:20:22 -0700

    adds Evidence Info component for evidence list

---
+++ src/components/EvidenceInfo.js
@@ -0,0 +1,8 @@
+export const EvidenceInfo = [
+  { id: 1, name: 'Ghost Writing', isChecked: false },
+  { id: 2, name: 'EMF Level 5', isChecked: false },
+  { id: 3, name: 'Ghost Orbs', isChecked: false },
+  { id: 4, name: 'Fingerprints', isChecked: false },
+  { id: 5, name: 'Freezing Temperatures', isChecked: false },
+  { id: 6, name: 'Spirit Box', isChecked: false }
+];
--- src/screens/EvidenceScreen.js
+++ src/screens/EvidenceScreen.js
@@ -2,15 +2,7 @@ import React, { useReducer } from 'react';
 import { View, StyleSheet, Text, Button } from 'react-native';
 import EvidenceCheckBox from '../components/EvidenceCheckBox';
 import GhostList from '../components/GhostList';
-
-const evidenceList = [
-  {id: 1, name: 'Ghost Writing', isChecked: false},
-  {id: 2, name: 'EMF Level 5', isChecked: false},
-  {id: 3, name: 'Ghost Orbs', isChecked: false},
-  {id: 4, name: 'Fingerprints', isChecked: false},
-  {id: 5, name: 'Freezing Temperatures', isChecked: false},
-  {id: 6, name: 'Spirit Box', isChecked: false}
-];
+import EvidenceInfo from '../components/EvidenceInfo'; 
 
 const reducer = (evidence, action) => {
   switch (action.type) {
@@ -25,7 +17,7 @@ const reducer = (evidence, action) => {
 };
 
 const EvidenceScreen = ({ navigation }) => {
-  const [evidence, dispatch] = useReducer(reducer, evidenceList);
+  const [evidence, dispatch] = useReducer(reducer, EvidenceInfo);
 
   return (
     <>

468e00f3cb05a8322d16562bfca628b4fbaca9ac
davidschlundtbodien <d.schlundtbodien@gmail.com>
2021-02-18 14:13:55 -0700

    adds remaining ghosts and global variables for Evidence

--- src/components/EvidenceInfo.js
+++ src/components/EvidenceInfo.js
@@ -1,8 +1,18 @@
+export const EVIDENCE = {
+  GHOST_WRITING: 'Ghost Writing',
+  EMF_LEVEL_5: 'EMF Level 5',
+  GHOST_ORBS: 'Ghost Orbs',
+  FINGERPRINTS: 'Fingerprints',
+  FREEZINGTEMPS: 'Freezing Temperatures',
+  SPIRIT_BOX: 'Spirit Box'
+};
+
+
 export const evidenceInfo = [
-  { id: 1, name: 'Ghost Writing', isChecked: false },
-  { id: 2, name: 'EMF Level 5', isChecked: false },
-  { id: 3, name: 'Ghost Orbs', isChecked: false },
-  { id: 4, name: 'Fingerprints', isChecked: false },
-  { id: 5, name: 'Freezing Temperatures', isChecked: false },
-  { id: 6, name: 'Spirit Box', isChecked: false }
+  { id: 1, name: EVIDENCE.GHOST_WRITING, isChecked: false },
+  { id: 2, name: EVIDENCE.EMF_LEVEL_5, isChecked: false },
+  { id: 3, name: EVIDENCE.GHOST_ORBS, isChecked: false },
+  { id: 4, name: EVIDENCE.FINGERPRINTS, isChecked: false },
+  { id: 5, name: EVIDENCE.SPIRIT_BOX, isChecked: false },
+  { id: 6, name: EVIDENCE.FREEZINGTEMPS, isChecked: false }
 ];

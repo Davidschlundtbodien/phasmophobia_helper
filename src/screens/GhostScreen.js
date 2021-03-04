67b5f42a2db7fca8b8fdb04fe736055cdff555a0
davidschlundtbodien <d.schlundtbodien@gmail.com>
2021-03-04 11:06:21 -0700

    adds a11y labels to Ghost screen

--- src/screens/GhostScreen.js
+++ src/screens/GhostScreen.js
@@ -9,10 +9,15 @@ const GhostScreen = ({ navigation }) => {
       <View style={globalStyles.ghostDetail}>
         <Text 
           style={globalStyles.ghostName}
+          accessibilityLabel={`Ghost name, ${ghost.name}`}
         >
           {ghost.name}
         </Text>
-        <View style={globalStyles.ghostEvidenceList}>
+        <View 
+          style={globalStyles.ghostEvidenceList}
+          accessible={true} 
+          accessibilityLabel={`Evidence for a ${ghost.name} are ${ghost.evidence[0]}, ${ghost.evidence[1]}, ${ghost.evidence[2]}`}
+        >
           {ghost.evidence.map((ev => {
             return (
               <Text 
@@ -27,13 +32,24 @@ const GhostScreen = ({ navigation }) => {
         <View style={globalStyles.ghostDetailList}>
           <Text 
             style={globalStyles.headerText}
+            accessibilityLabel={`A ${ghost.name}'s weakness is`}
           >
             Weakness
           </Text>
           <Text style={globalStyles.ghostWeakness}>{ghost.weakness}</Text>
-          <Text style={globalStyles.headerText}>Strength</Text>
+          <Text 
+            style={globalStyles.headerText}
+            accessibilityLabel={`A ${ghost.name}'s strength is`}
+          >
+            Strength
+          </Text>
           <Text style={globalStyles.ghostStrength}>{ghost.strength}</Text>
-          <Text style={globalStyles.headerText}>Journal Description</Text>
+          <Text 
+            style={globalStyles.headerText}
+            accessibilityLabel={`The Journal description of a ${ghost.name} is`}
+          >
+            Journal Description
+          </Text>
           <Text style={globalStyles.ghostDescription}>{ghost.description}</Text>
         </View>
       </View>
